4515a4caacc12d5491372ffa9017c1aa9d2a862c
User <putnin.ilya@gmail.com>
2016-02-20 03:41:54 +0200

    UPD //add languages

--- app/src/main/java/com/sourceit/task1/ui/ObjectType.java
+++ app/src/main/java/com/sourceit/task1/ui/ObjectType.java
@@ -3,6 +3,8 @@ package com.sourceit.task1.ui;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.util.ArrayList;
+
 public class ObjectType implements Parcelable {
     private String name;
     private String region;
@@ -11,23 +13,42 @@ public class ObjectType implements Parcelable {
     private String population;
     private String area;
 
+    ArrayList<String> languages;
 
-    protected ObjectType(Parcel in) {
-        name = in.readString();
-        region = in.readString();
-        subregion = in.readString();
-        capital = in.readString();
-        population = in.readString();
-        area = in.readString();
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.name);
+        dest.writeString(this.region);
+        dest.writeString(this.subregion);
+        dest.writeString(this.capital);
+        dest.writeString(this.population);
+        dest.writeString(this.area);
+        dest.writeSerializable(this.languages);
+    }
+
+    public ObjectType() {
+    }
+
+    private ObjectType(Parcel in) {
+        this.name = in.readString();
+        this.region = in.readString();
+        this.subregion = in.readString();
+        this.capital = in.readString();
+        this.population = in.readString();
+        this.area = in.readString();
+        this.languages = (ArrayList<String>) in.readSerializable();
     }
 
     public static final Creator<ObjectType> CREATOR = new Creator<ObjectType>() {
-        @Override
-        public ObjectType createFromParcel(Parcel in) {
-            return new ObjectType(in);
+        public ObjectType createFromParcel(Parcel source) {
+            return new ObjectType(source);
         }
 
-        @Override
         public ObjectType[] newArray(int size) {
             return new ObjectType[size];
         }
@@ -57,18 +78,11 @@ public class ObjectType implements Parcelable {
         return area;
     }
 
-    @Override
-    public int describeContents() {
-        return 0;
+    public ArrayList<String> getLanguages() {
+        return languages;
     }
 
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(name);
-        dest.writeString(region);
-        dest.writeString(subregion);
-        dest.writeString(capital);
-        dest.writeString(population);
-        dest.writeString(area);
+    public static Creator<ObjectType> getCREATOR() {
+        return CREATOR;
     }
 }
